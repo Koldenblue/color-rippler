28ac9326235b49c6df137ff3f032a0a4ca967cce
Kevin Miller <kmillergit@outlook.com>
2020-10-10 09:06:11 -0700

    cleaned up code

--- src/components/BoxGrid.js
+++ src/components/BoxGrid.js
@@ -3,45 +3,45 @@ import ColorBox from './Colors';
 import DynaColorBox from './DynaColorBox';
 
 function BoxGrid(props) {
-  const [red, setRed] = useState([]);
-  const [green, setGreen] = useState([]);
-  const [blue, setBlue] = useState([]);
-  const [positive, setPositive] = useState(1);
+  // const [red, setRed] = useState([]);
+  // const [green, setGreen] = useState([]);
+  // const [blue, setBlue] = useState([]);
+  // const [positive, setPositive] = useState(1);
   const [variance, setVariance] = useState(50);
 
-  const [colors, dispatch] = useReducer((state, action) => {
-    if (action === 'blue') {
-      if (state.blue > 254 || state.blue < 0) {
-         setPositive(positive * -1)
-        }
-      return { 'red': state.red, 'green': state.green, 'blue': state.blue + 5 * positive}
-    }
-    else if (action === 'red') {
-      if (state.red > 254 || state.red < 0) {
-         setPositive(positive * -1)
-        }
-      return { 'red': state.red + 5 * positive, 'green': state.green, 'blue': state.blue }
-    }
-    else if (action === 'green') {
-      if (state.green > 254 || state.green < 0) {
-        setPositive(positive * -1)
-      }
-      return { 'red': state.red, 'green': state.green + 5 * positive, 'blue': state.blue }
-    }
-  }, { 'red': 20, 'green': 80, 'blue': 40 })
+  // const [colors, dispatch] = useReducer((state, action) => {
+  //   if (action === 'blue') {
+  //     if (state.blue > 254 || state.blue < 0) {
+  //        setPositive(positive * -1)
+  //       }
+  //     return { 'red': state.red, 'green': state.green, 'blue': state.blue + 5 * positive}
+  //   }
+  //   else if (action === 'red') {
+  //     if (state.red > 254 || state.red < 0) {
+  //        setPositive(positive * -1)
+  //       }
+  //     return { 'red': state.red + 5 * positive, 'green': state.green, 'blue': state.blue }
+  //   }
+  //   else if (action === 'green') {
+  //     if (state.green > 254 || state.green < 0) {
+  //       setPositive(positive * -1)
+  //     }
+  //     return { 'red': state.red, 'green': state.green + 5 * positive, 'blue': state.blue }
+  //   }
+  // }, { 'red': 20, 'green': 80, 'blue': 40 })
 
 
-  useEffect(() => {
-    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
-      red.push(i);
-      green.push(i);
-      blue.push(i);
-    }
-    setRed(red);
-    setBlue(blue);
-    setGreen(green);
-    // now each color is an array of numbers, 0 thru 143
-  },[])
+  // useEffect(() => {
+  //   for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+  //     red.push(i);
+  //     green.push(i);
+  //     blue.push(i);
+  //   }
+  //   setRed(red);
+  //   setBlue(blue);
+  //   setGreen(green);
+  //   // now each color is an array of numbers, 0 thru 143
+  // },[])
 
 
   const GRID_SIZE = 12;
@@ -52,21 +52,12 @@ function BoxGrid(props) {
     colArr[i] = i;
   }
 
-
-  // let styles = {
-  //   color: {
-  //     backgroundColor: `rgb(${blueNum.red}, ${blueNum.green}, ${blueNum.blue})`,
-
-  //   }
+  // let dispatchArray = ['red', 'green', 'blue']
+  // const randomDispatch = () => {
+  //   let num = Math.floor(Math.random() * 3)
+  //   let randDispatch = dispatchArray[num]
+  //   return randDispatch;
   // }
-
-  let dispatchArray = ['red', 'green', 'blue']
-  const randomDispatch = () => {
-    let num = Math.floor(Math.random() * 3)
-    let randDispatch = dispatchArray[num]
-    return randDispatch;
-  }
-  let newDisp;
 
   let changeSurroundings = (value) => {
     console.log(value);
